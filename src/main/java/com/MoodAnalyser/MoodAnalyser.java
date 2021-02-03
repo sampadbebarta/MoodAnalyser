4283fe20ec7ce35c18e3ea4d04767138c4879152
Sampad <bebartasampad@gmail.com>
2021-02-03 14:16:16 +0530

    Mood Analysis check for all kind of user inputs

--- src/main/java/com/MoodAnalyser/MoodAnalyser.java
+++ src/main/java/com/MoodAnalyser/MoodAnalyser.java
@@ -1,15 +1,24 @@
 package com.MoodAnalyser;
 
 public class MoodAnalyser {
-	
+
 	public void PrintWelcomeMessage() {
 		System.out.println("Welcome to the Mood Analyser Problem");
 	}
-	
-	public String AnalyseMood(String message) {
-		if(message.contains("Sad"))
-		return "SAD";
-		return "HAPPY";
+
+	public String AnalyseMood(String message) throws MoodAnalysisException {
+		try {
+			if(message.length() == 0)
+				throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,"Please Enter Proper Non-Empty Mood");
+			if(message.contains("Sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch(NullPointerException e) {
+			throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,"Please Enter the Proper Mood");
+		} catch(IllegalArgumentException e) {
+			throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,"Please Enter the Proper Mood");
+		}
 	}
 
 }
--- src/test/java/com/MoodAnalyser/MoodAnalyserTest.java
+++ src/test/java/com/MoodAnalyser/MoodAnalyserTest.java
@@ -2,25 +2,70 @@ package com.MoodAnalyser;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-public class MoodAnalyserTest 
+import com.MoodAnalyser.MoodAnalysisException.ExceptionType;
+
+public class MoodAnalyserTest
 {
 	@Test
 	public void print_Welcome_Message() {
 		MoodAnalyser mood = new MoodAnalyser();
 		mood.PrintWelcomeMessage();
 	}
-	@Test 
+	@Test
 	public void Given_Message_When_Sad_Should_Return_Sad() {
 		MoodAnalyser moodAnalyser= new MoodAnalyser();
-		String mood = moodAnalyser.AnalyseMood("This is a Sad Message");
-		Assert.assertEquals("SAD", mood);
+		String mood = null;
+		try {
+			mood = moodAnalyser.AnalyseMood("This is a Sad Message");
+			Assert.assertEquals("SAD", mood);
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	@Test 
+	@Test
 	public void Given_Message_When_Happy_Should_Return_Happy() {
 		MoodAnalyser moodAnalyser= new MoodAnalyser();
-		String mood = moodAnalyser.AnalyseMood("This is a Happy Message");
-		Assert.assertEquals("HAPPY", mood);
+		String mood = null;
+		try {
+			mood = moodAnalyser.AnalyseMood("This is a Happy Message");
+			Assert.assertEquals("HAPPY", mood);
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
+
 	}
+
+	@Test
+	public void Given_Message_When_Null_Should_throw_Exception() {
+		MoodAnalyser moodAnalyser= new MoodAnalyser();
+		String mood = null;
+		try {
+			mood = moodAnalyser.AnalyseMood(null);
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals(ExceptionType.ENTERED_NULL, e.type );
+			Assert.assertEquals("Please Enter the Proper Mood", e.getMessage());
+		}
+
+	}
+
+	@Test
+	public void Given_Message_When_Empty_Should_throw_Exception() {
+		MoodAnalyser moodAnalyser= new MoodAnalyser();
+		String mood = null;
+		try {
+			mood = moodAnalyser.AnalyseMood("");
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals(ExceptionType.ENTERED_EMPTY, e.type );
+			Assert.assertEquals("Please Enter Proper Non-Empty Mood", e.getMessage());
+		}
+
+	}
+
+
+
+
 }
